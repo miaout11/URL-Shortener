340654966a76d100576dab1acf3311e6340ede0e
miaout11 <miaout.11@gmail.com>
2022-10-19 15:09:44 +0800

    feat: add mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,3 @@
-const express = require('express')
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_SHORT_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -11,14 +10,4 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`)
-})
+module.exports = db
