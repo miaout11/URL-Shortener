5f9ac07f7548cbee5a485c5fdf0dc03398e3ef8b
miaout11 <miaout.11@gmail.com>
2022-10-20 18:21:10 +0800

    add route to redirect newURL

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,19 @@ router.get('/', (req, res) => {
   res.render('index')
 });
 
+// use new short URL redirect
+router.get('/:shorten', (req, res) => {
+  const shortenName = req.params.shorten
+  Url.findOne({ shorten: shortenName })
+    .lean()
+    .then((relink) => {
+      if (relink) {
+        console.log(relink)
+        res.status(301).redirect(relink.originUrl)
+      }
+    })
+    .catch((error) => console.log(error))
+});
+
+
 module.exports = router;
